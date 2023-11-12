71d35cbbaebe2de1dd731d07a29910e309b91b56
ylamsiah <110131779+YuSuFLams@users.noreply.github.com>
2023-11-12 22:30:09 +0100

    ylamsiah

--- microshell.c
+++ microshell.c
@@ -12,9 +12,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <string.h>
 
 void ft_putchar(char c, int fd)
 {
@@ -24,55 +24,51 @@ void ft_putchar(char c, int fd)
 void ft_putstr(char *s, int fd)
 {
     if (!s)
-        write(fd, "(null)", 6);
+        ft_putstr("(null)", fd);
     else
         while (*s)
             ft_putchar(*s++, fd);
 }
 
-int ft_cd(char **s, int i)
+void ft_cd(char **s, int i)
 {
     if (i != 2)
-    {
         ft_putstr("error: cd: bad arguments", 2);
-    }
-    else if (chdir(s[1]) == -1)
+    else if (chdir(s[1]) != 0)
     {
         ft_putstr("error: cd: cannot change directory to ", 2);
         ft_putstr(s[1], 2);
     }
-	ft_putchar('\n', 2);
-    return EXIT_FAILURE;
+    ft_putchar('\n', 2);
 }
 
-int execut_cmd(char **s, char **envv, int tmp, int i)
+int execut_cmd(char **s, char **env, int tmp, int i)
 {
     s[i] = NULL;
     dup2(tmp, 0);
     close(tmp);
-    execve(s[0], s, envv);
+    execve(s[0], s, env);
     ft_putstr("error: cannot execute ", 2);
     ft_putstr(s[0], 2);
-    ft_putchar('\n', 2);
-    return EXIT_FAILURE;
+    return (ft_putchar('\n', 2), 1);
 }
 
 int main(int ac, char **str, char **envv)
 {
     int i, tmp, fd[2];
 
-	(void)ac;
     i = 0;
+    (void)ac;
     tmp = dup(0);
     while (str[i] && str[i + 1])
     {
         str = &str[i + 1];
         i = 0;
-        while (str[i] && (strcmp(str[i], "|") && strcmp(str[i], ";")))
+        while (str[i] && strcmp(str[i], ";") && strcmp(str[i], "|"))
             i++;
         if (!strcmp(str[0], "cd"))
             ft_cd(str, i);
-        else if (i && (!str[i] || !strcmp(";", str[i])))
+        else if (i && (!str[i] || !strcmp(str[i], ";")))
         {
             if (!fork())
             {
@@ -93,10 +89,9 @@ int main(int ac, char **str, char **envv)
             if (!fork())
             {
                 dup2(fd[1], 1);
-                close(fd[0]);
-                close(fd[1]);
+                close(fd[0]), close(fd[1]);
                 if (execut_cmd(str, envv, tmp, i))
-                    return 1;
+                    return (1);
             }
             else
             {
@@ -106,5 +101,5 @@ int main(int ac, char **str, char **envv)
             }
         }
     }
-	return (close(tmp), 0);
+    return (close(tmp), 0);
 }
